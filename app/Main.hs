54667992ed65fd57af83d3bbeccb4daea3cff338
Samuel Klumpers <samuelhklumpers@gmail.com>
2021-10-31 14:56:40 +0100

    vectors for movement and collision

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+
 import Graphics.Gloss
+
 
 import GameState
 
--- src/Being.hs
+++ src/Being.hs
@@ -1,3 +1,48 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+
+
 module Being where
 
-data Being = Being
+
+import Data.Vector.Unboxed.Sized as VS
+
+import Control.Lens
+import GHC.TypeNats (KnownNat)
+
+
+type R2 = Vector 2 Float
+
+data Phys = Phys {_pos :: R2, _vel :: R2, _mass :: Float, _radius :: Float}
+makeLenses ''Phys
+
+data Race = Player | Asteroid | Bullet | Enemy
+
+data Being = Being {_phys :: Phys, _race :: Race}
+makeLenses ''Being
+
+
+(*|) :: (Unbox a, Num a, KnownNat d) => a -> Vector d a -> Vector d a
+m *| v = VS.map (*m) v
+
+norm :: (Unbox a, Floating a, KnownNat d) => Vector d a -> a
+norm = sqrt . VS.foldr ((+) . (^2)) 0
+
+-- move assuming no gravity or collision
+freeFall :: Float -> Being -> Being
+freeFall dt = phys %~ freeFall' dt
+
+freeFall' :: Float -> Phys -> Phys
+freeFall' dt p = pos +~ dt *| (p ^. vel) $ p 
+
+-- Just (collision vector) when colliding, otherwise None
+-- TODO need posteriori collision detection to prevent bullets teleporting through thin surfaces
+collide :: Being -> Being -> Maybe R2
+collide a b
+    | d1 <= d2  = Just v
+    | otherwise = Nothing where
+        v = (a ^. phys . pos) - (b ^. phys . pos)
+        d1 = norm v
+        d2 = (a ^. phys . radius) + (b ^. phys . radius)
+          
