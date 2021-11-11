69c812ac0f3fa63b57c79e992993a9ba3528c140
Freek98 <f.h.d.arts@gmail.com>
2021-11-11 11:07:24 +0100

    nu met Maybe

--- src/Shooting.hs
+++ src/Shooting.hs
@@ -5,16 +5,17 @@ import Graphics.Gloss.Interface.IO.Interact
 import qualified Graphics.Gloss.Data.Point.Arithmetic as Vec
 import Control.Lens
 
-import Util
 import Being
 
 data Firing = NoShots | Shot Vector
 
+{-
 processfire :: Event -> Firing
 processfire (EventKey (MouseButton LeftButton ) _ _ mousepos) = Shot mousepos
 processfire _ = NoShots
+-}
 
-
+{-
 shootBullet :: Being -> Vector -> Being
 shootBullet shooter targetpos = makeBeing Bullet startpos velocity where
                                  shooterpos = shooter ^. phys. pos
@@ -23,3 +24,17 @@ shootBullet shooter targetpos = makeBeing Bullet startpos velocity where
                                  velocity   = bulletspeed `mulSV` direction
                                  startpos   = shooterpos  Vec.+ ((shooter ^. phys.radius) `mulSV` direction )
                                 
+-}
+
+
+
+shootBullet :: Being -> Vector -> Maybe Being
+shootBullet shooter targetpos  | time < 0.3 = Nothing
+                               | otherwise  = Just(makeBeing Bullet startpos velocity) where
+                                   time = shooter ^. timeSinceLastShot
+                                   shooterpos = shooter ^. phys. pos
+                                   direction  = normalizeV (targetpos Vec.- shooterpos)
+                                   bulletspeed= 22
+                                   velocity   = bulletspeed `mulSV` direction
+                                   startpos   = shooterpos  Vec.+ ((shooter ^. phys.radius) `mulSV` direction )
+                                                                
