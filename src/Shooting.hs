ac0758448acfa3be25f99316775bdcd88abb377c
Freek98 <f.h.d.arts@gmail.com>
2021-11-11 12:11:50 +0100

    dit crasht nu, poging firestep te implementen maar dan goed

--- src/Shooting.hs
+++ src/Shooting.hs
@@ -4,10 +4,14 @@ import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Interface.IO.Interact
 import qualified Graphics.Gloss.Data.Point.Arithmetic as Vec
 import Control.Lens
+import Control.Monad.State
 
 import Being
+import WorldInit
 
-data Firing = NoShots | Shot Vector
+shoottimeout = 0.3
+bulletspeed= 22
+--data Firing = NoShots | Shot Vector
 
 {-
 processfire :: Event -> Firing
@@ -20,7 +24,6 @@ shootBullet :: Being -> Vector -> Being
 shootBullet shooter targetpos = makeBeing Bullet startpos velocity where
                                  shooterpos = shooter ^. phys. pos
                                  direction  = normalizeV (targetpos Vec.- shooterpos)
-                                 bulletspeed= 22
                                  velocity   = bulletspeed `mulSV` direction
                                  startpos   = shooterpos  Vec.+ ((shooter ^. phys.radius) `mulSV` direction )
                                 
@@ -29,12 +32,30 @@ shootBullet shooter targetpos = makeBeing Bullet startpos velocity where
 
 
 shootBullet :: Being -> Vector -> Maybe Being
-shootBullet shooter targetpos  | time < 0.3 = Nothing
-                               | otherwise  = Just(makeBeing Bullet startpos velocity) where
-                                   time = shooter ^. timeSinceLastShot
+--In principe kan alles schieten, leuk als je enemies maakt die turrets kunnen plaatsen op asteroids zodat asteroids op de player schieten. 
+shootBullet shooter targetpos  | time < shoottimeout = Nothing
+                               | otherwise    = Just(makeBeing Bullet startpos velocity) where
+                                   time       = shooter ^. timeSinceLastShot
                                    shooterpos = shooter ^. phys. pos
                                    direction  = normalizeV (targetpos Vec.- shooterpos)
-                                   bulletspeed= 22
                                    velocity   = bulletspeed `mulSV` direction
                                    startpos   = shooterpos  Vec.+ ((shooter ^. phys.radius) `mulSV` direction )
-                                                                
+
+playerShot :: World -> Maybe Being
+playerShot w = do  target <- (w ^.userIn.firing)
+                   bullet <- shootBullet (w^.beings.player) target
+                   return bullet
+
+fireStep :: Float -> World -> World
+fireStep dt = execState $ do    
+                        w <- get
+                        case (playerShot w) of 
+                          Nothing     -> ( (beings.player.timeSinceLastShot) +~ dt)
+                          Just bullet -> do ((beings.player.timeSinceLastShot).= 0)
+                                            spawnBeing bullet
+                          
+                    
+                        
+                        
+
+
